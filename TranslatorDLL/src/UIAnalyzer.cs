b263ee2dd7d5f9ff3f60f6d3915d3c57e8a6c78c
Tsygan <nikolay.kamenskiy@list.ru>
2025-05-09 15:17:02 +0300

    .

--- TranslatorDLL/src/UIAnalyzer.cs
+++ TranslatorDLL/src/UIAnalyzer.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Debug = UnityEngine.Debug;
+using Debug = UnityEngine.UnityEngine.Debug;
 
 namespace GreyHackTranslator
 {
